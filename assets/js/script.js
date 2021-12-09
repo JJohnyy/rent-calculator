8112e91bc2285436290d5dc8a79015e170ee87f5
JJohnyy <84328224+JJohnyy@users.noreply.github.com>
2021-12-09 15:43:37 +0100

    css,, html, javascript
    changed name of css, updated html, css and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,70 +3,10 @@ const roomsBtnsRef = Array.from(document.querySelectorAll(".rooms-btn"));
 const prague1 = document.querySelector("#pOne");
 const room1 = document.querySelector("#oneBedroom");*/
 
-
-/*const prague = [
-    {
-        id: "pOne",
-        location: "Prague 1",
-        oneBedroom: 700,
-        twoBedroom: 650,
-        threeBedroom: 600,
-        fourBedroom: 550,
-    },
-    {
-        id: "pTwo",
-        location: "Prague 2",
-        oneBedroom: 670,
-        twoBedroom: 620,
-        threeBedroom: 570,
-        fourBedroom: 520,
-    },
-    {
-        id: "pThree",
-        location: "Prague 3",
-        oneBedroom: 620,
-        twoBedroom: 550,
-        threeBedroom: 470,
-        fourBedroom: 400,
-    },
-    {
-        id: "pFour",
-        location: "Prague 4",
-        oneBedroom: 580,
-        twoBedroom: 530,
-        threeBedroom: 470,
-        fourBedroom: 410,
-    }
-]
-
-locationBtnsRef.forEach(button => {
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-        matchLocation(event.target.id);
-    })
-});
-
-function matchLocation(id) {
-    const location = prague.filter(location => {
-        if (location.id == id) {
-            return location
-        }
-    })
-    console.log(location)
-}
-
-
-roomsBtnsRef.forEach(button => {
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-        matchBedroom(event.target.id);
-    })
-});
-
-
-function matchBedroom(id) {
-    
-}*/
+const parkingLabel = document.querySelector("#parking-label");
+const furnishedLabel = document.querySelector("#furnished-label");
+const parking = document.getElementById("parking");
+const furnished = document.getElementById("furnished");
 
 
 //store rent prices
@@ -148,7 +88,7 @@ function changeColor() {
  * changes color of second row buttons
  */
 function changeColoRooms() {
-    if (this.style.backgroundColor === "r#ff751a") {
+    if (this.style.backgroundColor === "#ff751a") {
         this.style.backgroundColor = "#0047b3";
     } else {
         this.style.backgroundColor = "#ff751a";
@@ -165,8 +105,6 @@ function changeColoRooms() {
  * returns final price
  */
 function appliances() {
-    let parking = document.getElementById("parking");
-    let furnished = document.getElementById("furnished");
     if (parking.checked == true && furnished.checked == true) {
         return floorSize() + 1500;
     } else if (parking.checked == true && furnished.checked !== true) {
@@ -177,6 +115,15 @@ function appliances() {
         return floorSize();
     }
 }
+
+
+function changeColorParking() {
+    
+        parkingLabel.classList.add("active");
+   
+}
+
+parkingLabel.addEventListener("click", changeColorParking)
 
 
 
